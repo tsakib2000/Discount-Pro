69e2f28db31c937554f06283826fd5bb2215e546
tsakib2000 <isakib49@gmail.com>
2024-11-18 20:19:56 +0600

    Authentication context api created

--- src/Components/WelcomeText.jsx
+++ src/Components/WelcomeText.jsx
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+
 const WelcomeText = () => {
+const {name}=useContext(AuthContext)
   return (
     <div className="flex justify-center items-center text-center flex-col mt-3">
       <h1 className="text-3xl md:text-5xl font-bold">Welcome to Discount Pro! 🎉</h1>
@@ -6,6 +10,7 @@ const WelcomeText = () => {
         Your ultimate destination for the best deals and exclusive coupons from
         Bangladesh's top e-commerce platforms. 🛒
       </p>
+      <p>{name}</p>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Router'
+import AuthProvider from './AuthProvider/AuthProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router} />
+ <AuthProvider>
+ <RouterProvider router={router} />
+ </AuthProvider>
   </StrictMode>,
 )
